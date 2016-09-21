b963dd30ceda7892f781c0c7ee1b277aec99e44d
nadiaforu <nadiaforero31@gmail.com>
2016-09-21 13:52:05 +0200

    Presentacion

--- Sumar.java
+++ Sumar.java
@@ -1,1 +1,25 @@
 package modelo;
+
+public class Calculadora {
+    
+    int num1, num2;
+    
+    public Calculadora(int a, int b){
+        num1=a;
+        num2=b;
+    }
+    public Calculadora(int n){
+        num1=n;
+        num2=n;
+    }
+    public Calculadora(){
+        num1=1;
+        num2=10;
+    }
+
+public void fijarNum1(int n){
+        num1=n;
+    }
+    public void fijarNum2(int n){
+        num2=n;
+    }
